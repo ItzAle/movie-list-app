cf0f012c1eadc3087c79c1ea605a58d51ded1314
alejandro <alexcs9230@gmail.com>
2023-02-02 12:03:21 +0100

    added and styled images

--- src/components/catalogue/catalogue.jsx
+++ src/components/catalogue/catalogue.jsx
@@ -1,25 +1,30 @@
-import React, {useEffect, useState} from 'react'
-import productServices from '../../apiServices/productServices';
-import style from './catalogue.module.css'
+import React, { useEffect, useState } from "react";
+import productServices from "../../apiServices/productServices";
+import style from "./catalogue.module.css";
 
-const pelisData = [{id: 1 , name: "peli1" , img: "algo.jpg"} ,{id: 2 , name: "peli2" , img: "algo.jpg"}, {id: 3 , name: "peli3" , img: "algo.jpg"} ]
+const pelisData = [
+  { id: 1, name: "peli1", img: "algo.jpg" },
+  { id: 2, name: "peli2", img: "algo.jpg" },
+  { id: 3, name: "peli3", img: "algo.jpg" },
+];
 
 function Catalogue() {
-  const [pelis, setPelis] = useState([]) 
-  
-  useEffect(()=>{
-    productServices.getAll().then(data=>setPelis(data))
-  // setPelis(pelisData)
-}, []);
+  const [pelis, setPelis] = useState([]);
+
+  useEffect(() => {
+    productServices.getAll().then((data) => setPelis(data));
+    // setPelis(pelisData)
+  }, []);
 
   return (
-    <div>
-      <div className={style.card}>
-      {pelis.map(peli=><li>{peli.movieName}</li>)}
+    <div className={style.card}>
+      <div className={style.img}>
+        {pelis.map((peli) => (
+          <img src={peli.movieURL} alt="img" />
+        ))}
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Catalogue
+export default Catalogue;
